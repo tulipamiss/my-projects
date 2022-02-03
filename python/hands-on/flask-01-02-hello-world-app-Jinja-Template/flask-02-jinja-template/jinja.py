99af7ff4d2cd07ce70d9c7e09dcdf1f1d3fdca9c
Lale Arslan <slase74@gmail.com>
2022-02-03 16:40:33 +0300

    repoya eklendi

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -9,7 +9,7 @@ def head():
 @app.route('/mult')
 def number():
     var1, var2 = 30, 70
-    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+    return render_template('body.html', num1 = var1 , num2 = var2, multiplication = var1*var2)
 
 
 
